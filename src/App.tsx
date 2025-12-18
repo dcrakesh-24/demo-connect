833ed1ab9c38a09949896b2be8cee9d155f4c3c0
dcrakesh-24 <rakeshmudhaliyar@gmail.com>
2025-12-18 11:41:24 +0530

    took pull

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,9 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-<<<<<<< HEAD
-=======
 import CompanyProfile from "./pages/CompanyProfile";
 import Template1 from "./pages/landing/Template1";
 import Template2 from "./pages/landing/Template2";
->>>>>>> 6f99e52eb04604e85f891dc78deb0ded18e762fe
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -22,12 +19,9 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-<<<<<<< HEAD
-=======
           <Route path="/company/:companyId" element={<CompanyProfile />} />
           <Route path="/template-1" element={<Template1 />} />
           <Route path="/template-2" element={<Template2 />} />
->>>>>>> 6f99e52eb04604e85f891dc78deb0ded18e762fe
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
